07cafc928d98966a5875aa1d8e59ca3de2ccb628
Anton Filippovich <antonfilippovich.job@gmail.com>
2024-09-13 18:46:36 +0300

    chore: setup speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -23,6 +24,8 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  <SpeedInsights/>
+  
   return (
     <html lang="en">
       <body
