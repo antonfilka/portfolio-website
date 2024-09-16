db4e460caa5c8be9c06aef1ecbaca7abc08a9b98
Anton Filippovich <antonfilippovich.job@gmail.com>
2024-09-16 16:58:25 +0300

    fix: change domain name

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: "/private/",
     },
-    sitemap: "https://af-shots-portfolio.vercel.app/sitemap.xml",
+    sitemap: "https://antonfilka.by/sitemap.xml",
   };
 }
